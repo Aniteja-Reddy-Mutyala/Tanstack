f2524ea4c063971cba21a683cd3d65e3febb2b3f
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-12-12 15:12:58 -0800

    Used UseQueries

--- github-app/src/App.tsx
+++ github-app/src/App.tsx
@@ -3,15 +3,43 @@ import {
   QueryClient,
   QueryClientProvider,
   useQuery,
+  useQueries
 } from "@tanstack/react-query";
 const queryClient = new QueryClient();
-function GitHubProfile(){
-  return <div >Github profile component</div>
+const fetchGitHubUser= async(user: any)=>{
+  const response=await fetch(`https://api.github.com/users/${user}`)
+  if (!response.ok){
+    throw new Error("error in fetching the response")
+  }
+  return response.json();
+}
+function GitHubProfiles(){
+  const usernames=["Aniteja-Reddy-Mutyala","moontahoe"]
+  const results=useQueries({
+    queries:usernames.map(user=>({
+      queryKey:["user",user],
+      queryFn:()=>fetchGitHubUser(user)
+    }))
+  })
+  // console.log(data)
+  // if (isLoading){
+  //   return <div>Loading!!!!!</div>
+  // }
+  // if(isError){
+  //   return <div>Error!!!!</div>
+  // }
+  return (
+    <>
+  <div >Github profile component</div>
+   {JSON.stringify(results)}
+   
+  </>
+  )
 }
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <GitHubProfile/>
+      <GitHubProfiles/>
     </QueryClientProvider>
   );
 }
