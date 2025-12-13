4c809156374a11a34eec7d4fe4e28c261c13d4f9
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-12-12 17:06:29 -0800

    Used css

--- github-app/src/App.tsx
+++ github-app/src/App.tsx
@@ -4,24 +4,24 @@ import {
   QueryClient,
   QueryClientProvider,
   useQuery,
-  useQueries
+  useQueries,
 } from "@tanstack/react-query";
 const queryClient = new QueryClient();
-const fetchGitHubUser= async(user:string)=>{
-  const response=await fetch(`https://api.github.com/users/${user}`)
-  if (!response.ok){
-    throw new Error("error in fetching the response")
+const fetchGitHubUser = async (user: string) => {
+  const response = await fetch(`https://api.github.com/users/${user}`);
+  if (!response.ok) {
+    throw new Error("error in fetching the response");
   }
   return response.json();
-}
-function GitHubProfiles(){
-  const usernames=["Aniteja-Reddy-Mutyala","moontahoe"]
-  const results=useQueries({
-    queries:usernames.map(user=>({
-      queryKey:["user",user],
-      queryFn:()=>fetchGitHubUser(user)
-    }))
-  })
+};
+function GitHubProfiles() {
+  const usernames = ["Aniteja-Reddy-Mutyala", "moontahoe"];
+  const results = useQueries({
+    queries: usernames.map((user) => ({
+      queryKey: ["user", user],
+      queryFn: () => fetchGitHubUser(user),
+    })),
+  });
   // console.log(data)
   // if (isLoading){
   //   return <div>Loading!!!!!</div>
@@ -32,46 +32,51 @@ function GitHubProfiles(){
   // setTimeout(()=>{
   //   console.log("")
   // },500000000);
-  interface Favourites{
-    [username:string]:boolean
+  interface Favourites {
+    [username: string]: boolean;
   }
-  const [favourites,setFavourites]=useState<Favourites>({})
-  
-  const toggleFavourite=(user:string):void=>{
-    setFavourites(prev=>({
-      ...prev,[user]:!prev[user]
-    }
+  const [favourites, setFavourites] = useState<Favourites>({});
 
-    ))
+  const toggleFavourite = (user: string): void => {
+    setFavourites((prev) => ({
+      ...prev,
+      [user]: !prev[user],
+    }));
+  };
+  const isLoading = results.some((query) => query.isLoading);
+  if (isLoading) {
+    return <div>Loading.....</div>;
   }
-  const isLoading=results.some(query=>query.isLoading)
-  if (isLoading){
-    return <div>Loading.....</div>
-  }
-  
-  
+
   return (
     <>
-  <div >Github profile component</div>
-   {results.map((user:any)=>{
-    if (!user.data){
-      return null;
-    }
-    return ( <div key={user.data.login}>
-    <img src={user.data.avatar_url} alt={user.data.login}/>
-    <h2>{user.data.login}</h2>
-    <button onClick={()=>toggleFavourite(user.data.login)}>{favourites[user.data.login]?"*Favourited":"* add to favourites"}</button>
-    </div>)
-   
-})}
-   
-  </>
-  )
+      <div className="profile-conatiner">
+        
+      {results.map((user: any) => {
+        if (!user.data) {
+          return null;
+        }
+        return (
+          
+          <div key={user.data.login} className="profile-card">
+            <img src={user.data.avatar_url} className="profile-avatar" alt={user.data.login} />
+            <h2>{user.data.login}</h2>
+            <button onClick={() => toggleFavourite(user.data.login)}>
+              {favourites[user.data.login]
+                ? "*Favourited"
+                : "* add to favourites"}
+            </button>
+          </div>
+        );
+      })}
+      </div>
+    </>
+  );
 }
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <GitHubProfiles/>
+      <GitHubProfiles />
     </QueryClientProvider>
   );
 }
